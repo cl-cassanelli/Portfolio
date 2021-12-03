64944b3dff1f4fbb1ea4e17af4da330a8afa196a
cl_cassanelli <erbapippo155@gmail.com>
2021-12-04 00:53:30 +0100

    Update

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,53 @@
-document.querySelector('.hamb').addEventListener("click", nav);
+const nav = document.querySelector('.mobile-menu').addEventListener('click', click);
+const cA = document.querySelector('#c').addEventListener('click', c);
+const pA = document.querySelector('#p').addEventListener('click', p);
 
-function nav(){
-    const cambio4 = document.querySelector('nav')
-    cambio4.classList.toggle('navmod')
+
+function click() {
+    var cambio1 =  document.querySelector('.hamb span:first-of-type');
+    var cambio2 =  document.querySelector('.hamb span:nth-of-type(2)');
+    var cambio3 =  document.querySelector('.hamb span:last-of-type');
+    if(document.querySelector('ul').style.left == "0%"){
+        document.querySelector('ul').style.left = "-100%"
+        cambio1.classList.remove('hamb_span1');
+        cambio2.classList.remove('hamb_span2');
+        cambio3.classList.remove('hamb_span3');
+    }else{
+        document.querySelector('ul').style.left = "0%"
+        cambio1.classList.toggle('hamb_span1');
+        cambio2.classList.toggle('hamb_span2');
+        cambio3.classList.toggle('hamb_span3');
+    }    
+}
+function c() {
+    var cambio1 =  document.querySelector('.hamb span:first-of-type');
+    var cambio2 =  document.querySelector('.hamb span:nth-of-type(2)');
+    var cambio3 =  document.querySelector('.hamb span:last-of-type');
+    if(document.querySelector('ul').style.left == "0%"){
+        document.querySelector('ul').style.left = "-100%"
+        cambio1.classList.remove('hamb_span1');
+        cambio2.classList.remove('hamb_span2');
+        cambio3.classList.remove('hamb_span3');
+    }else{
+        document.querySelector('ul').style.left = "0%"
+        cambio1.classList.toggle('hamb_span1');
+        cambio2.classList.toggle('hamb_span2');
+        cambio3.classList.toggle('hamb_span3');
+    } 
+}
+function p() {
+    var cambio1 =  document.querySelector('.hamb span:first-of-type');
+    var cambio2 =  document.querySelector('.hamb span:nth-of-type(2)');
+    var cambio3 =  document.querySelector('.hamb span:last-of-type');
+    if(document.querySelector('ul').style.left == "0%"){
+        document.querySelector('ul').style.left = "-100%"
+        cambio1.classList.remove('hamb_span1');
+        cambio2.classList.remove('hamb_span2');
+        cambio3.classList.remove('hamb_span3');
+    }else{
+        document.querySelector('ul').style.left = "0%"
+        cambio1.classList.toggle('hamb_span1');
+        cambio2.classList.toggle('hamb_span2');
+        cambio3.classList.toggle('hamb_span3');
+    } 
 }
--- php/language-detector.php
+++
@@ -1,6 +0,0 @@
-<?php
-  if ($_SERVER["HTTP_ACCEPT_LANGUAGE"] == "it")
-    header("location: ../index.html");
-  else 
-    header("location: ../en.html");
-?>
