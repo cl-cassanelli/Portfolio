f55682c94832507fbbb5cb5409b3706001f3e90f
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2024-04-13 12:50:07 +0200

    Update test

--- script/php/mail.php
+++ script/php/mail.php
@@ -1,19 +1,27 @@
-<?php 
+<?php
 
-    header("Access-Control-Allow-Origin: *");
+$allowed_referer = "https://cl-cassanelli.github.io/Portfolio/";
+
+// Verifica se il referer è quello autorizzato
+if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] === $allowed_referer) {
+    // Imposta gli header CORS per consentire solo il referer autorizzato
+    header("Access-Control-Allow-Origin: " . $allowed_referer);
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-
 
     $name = $_GET["name"];
     $email = $_GET["email"];
     $object = $_GET["object"];
 
-    $oggetto = $object."\n\nMail inviata da: ".$name; 
+    $oggetto = $object . "\n\nMail inviata da: " . $name;
 
-    if(mail("cldevofficial@gmail.com", "Nuova mail - giuseppecassanelli.it", $oggetto)) {
+    if (mail("cldevofficial@gmail.com", "Nuova mail - giuseppecassanelli.it", $oggetto)) {
         echo "c'è l'abbiamo fattaaaa";
     } else {
         echo "diocane";
     }
-
+} else {
+    // Restituisci un errore 403 Forbidden se il referer non è autorizzato
+    http_response_code(403);
+    echo "Accesso non autorizzato";
+}
 ?>
